f154545080737b3cf923bd978dc633eb4212ed2d
R-Dharun <Dharun@users.noreply.github.com>
2024-04-27 15:04:50 +0530

    commit

--- client3.py
+++ client3.py
@@ -35,16 +35,17 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = (bid_price + ask_price)/2
+    price = (bid_price + ask_price) / 2 # Compute the stock price using the given formula
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    if price_b == 0:
-        return
     """ ------------- Update this function ------------- """
-    return price_a/price_b
+    if (price_b == 0):
+        return 
+    else:
+        return price_a / price_b
 
 
 # Main
@@ -54,10 +55,10 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
-        prices ={}
+        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            prices[stock] =  price
+            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
+        print("Ratio %s" % getRatio(price, price))
--- client_test.py
+++ client_test.py
@@ -1,5 +1,6 @@
 import unittest
 from client3 import getDataPoint
+from client3 import getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +9,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    dataPoint = ('ABC', 120.48, 121.2, 120.48)  # Expected data point
+    self.assertEqual(getDataPoint(quotes[0]), dataPoint)  # Assert equality of computed data point and expected data point
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,10 +18,23 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    dataPoint = ('ABC', 120.48, 119.2, 120.48)  # Expected data point where bid price is greater than ask price
+    self.assertEqual(getDataPoint(quotes[0]), dataPoint)  # Assert equality of computed data point and expected data point
 
 
   """ ------------ Add more unit tests ------------ """
-
+  def test_getRatio_when_price_b_not_zero(self):
+    """ Test when price_b is not zero """
+    price_a = 20
+    price_b = 10
+    expected_ratio = 2
+    self.assertEqual(getRatio(price_a, price_b), expected_ratio)
+    
+  def test_getRatio_when_price_b_zero(self):
+    """ Test when price_b is zero """
+    price_a = 10
+    price_b = 0
+    self.assertIsNone(getRatio(price_a, price_b))
 
 
 if __name__ == '__main__':
